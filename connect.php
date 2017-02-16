1d1b3159c8886290673dca1c009fc800c11d2b71
Anna Angelova <angelowwa@gmail.com>
2017-02-16 19:54:08 +0200

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+// Connection to a MySQL database:
+
+$dbc = @mysqli_connect('localhost', 'root', '', 'cinvoice_Iwriteoff') OR die('Could not connect to the database: ' .mysqli_connect_error());
+
+
+?>
